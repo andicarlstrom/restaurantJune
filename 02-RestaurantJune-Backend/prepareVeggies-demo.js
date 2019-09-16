3f89596f55d4623885da227f394053d70ea0f433
fkakatie <sextonskatie@gmail.com>
2019-09-16 17:59:05 -0600

    Reordered blank prepareVeggies-demo

--- 02-RestaurantJune-Backend/prepareVeggies-demo.js
+++ 02-RestaurantJune-Backend/prepareVeggies-demo.js
@@ -1,25 +1,29 @@
 // Store all of the veggies supplied from the CLI that the chef wants to have prepared
 
-
-// Function in charge of receiving (1) veggies from the chef and (2) the method the chef wants the veggies to be prepared with.
-
-    // Array that will hold the prepared veggies
-
     
-    // Loop that takes each veggie and prepares them with the prepMethod 
-    // THEN adds the prepped veggie to the preppedVeggies array
-       
-    // Finally we return the fully prepared array of veggies
-    
-
 // Function to chop veggies
 
 
 // Function to dice veggies
 
 
+// Function in charge of receiving (1) veggies from the chef and (2) the method the chef wants the veggies to be prepared with.
+
+
+    // Array that will hold the prepared veggies
+
+    
+    // Loop that takes each veggie and prepares them with the prepMethod 
+    // THEN adds the prepped veggie to the preppedVeggies array
+
+       
+    // Finally we return the fully prepared array of veggies
+
+
 // Variable that WILL hold the chopped veggies
 
+
 // Variable that WILL hold the diced veggies
+
 
 
